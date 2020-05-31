2efcf1d2f8249c5e58f91154b9bd337fd74ce7d4
grepnikhil <nikhilj50@gmail.com>
2020-05-31 12:18:23 +0530

    formatted files

--- DailyHunt.py
+++ DailyHunt.py
@@ -1,24 +1,24 @@
-#Coded By Gowtham on 30/05/2020 
-#Coded Using Vim Text Editor
+# Coded By Gowtham on 30/05/2020
+# Coded Using Vim Text Editor
 from NewsData import newsData
 queryDict = {
-        'science':'sci',
-        'technology':'tech',
-        'india':'ind',
-        'automobile':'auto',
-        'entertainment':'ent',
-        'sport':'spt',
-        'world':'wrd',
-        'lifestyle':'life',
-        'employment':'emp',
-        'religion':'rel',
-        'football':'foot',
-        'movies':'mov'
-        }
+    'science': 'sci',
+    'technology': 'tech',
+    'india': 'ind',
+    'automobile': 'auto',
+    'entertainment': 'ent',
+    'sport': 'spt',
+    'world': 'wrd',
+    'lifestyle': 'life',
+    'employment': 'emp',
+    'religion': 'rel',
+    'football': 'foot',
+    'movies': 'mov'
+}
+
+
 def getNews(category):
 
     query = category.lower()
 
     return newsData(queryDict[query])
-
-
--- genUrl.py
+++ genUrl.py
@@ -1,25 +1,27 @@
-#Coded By Gowtham on 30/05/2020 
-#Coded Using Vim Text Editor
+# Coded By Gowtham on 30/05/2020
+# Coded Using Vim Text Editor
 import requests
 from bs4 import BeautifulSoup as bs 
 import base64
 
+
 def getUrl(key):
-    queries = ["science-topics-20","technology-topics-102","india-topics-15","automobile-topics-101","entertainment-topics-8","sports-topics-17","world-topics-16","lifestyle-topics-11","employment-topics-136","religion+spirituality-topics-135","football-topics-495","movie+review-topics-807","buzz+trending-topics-791"]
+    queries = ["science-topics-20", "technology-topics-102", "india-topics-15", "automobile-topics-101", "entertainment-topics-8", "sports-topics-17", "world-topics-16",
+               "lifestyle-topics-11", "employment-topics-136", "religion+spirituality-topics-135", "football-topics-495", "movie+review-topics-807", "buzz+trending-topics-791"]
 
     queryDict = {
-            'sci':queries[0],
-            'tech':queries[1],
-            'ind':queries[2],
-            'auto':queries[3],
-            'ent':queries[4],
-            'spt':queries[5],
-            'wrd':queries[6],
-            'life':queries[7],
-            'emp':queries[8],
-            'rel':queries[9],
-            'foot':queries[10],
-            'mov':queries[11]
+        'sci': queries[0],
+        'tech': queries[1],
+        'ind': queries[2],
+        'auto': queries[3],
+        'ent': queries[4],
+        'spt': queries[5],
+        'wrd': queries[6],
+        'life': queries[7],
+        'emp': queries[8],
+        'rel': queries[9],
+        'foot': queries[10],
+        'mov': queries[11]
             }
 
     query = queryDict[key]
@@ -39,5 +41,3 @@ def getUrl(key):
     URL = dataDecoded.decode('utf-8')
 
     return URL
-
-
